a9ce65580396018fc032e98c73c6979312f91c33
Michael Verdi <michael.v.verdi@gmail.com>
2019-05-02 12:22:10 -0400

    add cors filtering for localhost:3000

--- src/app.js
+++ src/app.js
@@ -13,8 +13,22 @@ const morganOption = (NODE_ENV === 'production')
   : 'common';
 
 app.use(morgan(morganOption));
-app.use(cors());
 app.use(helmet());
+
+// whitelist allowed origins
+const allowedOrigins = ['http://localhost:3000', 'http://my-prod-client-app-url'];
+app.use(cors({
+  origin: function(origin, callback){
+    // allow requests with no origin - like mobile apps or curl requests
+    if(!origin) return callback(null, true);
+    if(allowedOrigins.indexOf(origin) === -1){
+      const msg = 'The CORS policy for this site does not ' +
+                  'allow access from the specified Origin.';
+      return callback(new Error(msg), false);
+    }
+    return callback(null, true);
+  }
+}));
 
 app.get('/', (req, res) => {
   res.send('Hello World!');
